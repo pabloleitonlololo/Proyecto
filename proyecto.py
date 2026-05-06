f29d3862529c68ecf7af0d0268338f53473dbf4a
pabloleitonlololo <tu-email-de-pablo@gmail.com>
2026-05-06 08:11:47 -0600

    colores por objet y acualizacion selecionar objetoso

--- proyecto.py
+++ proyecto.py
@@ -4,6 +4,13 @@ from ultralytics import YOLO
 
 model = YOLO("yolov8n.pt")
 
+colores = {
+    "car": (255, 0, 255),
+    "bicycle": (255, 0, 0),
+    "traffic light": (0, 165, 255),
+    "person": (0, 0, 255)
+}
+
 objetos_disponibles = ["car", "bicycle", "traffic light", "person"]
 
 print("1. Detectar todos")
@@ -15,7 +22,7 @@ opcion = input("Seleccionar una opción: ")
 if opcion == "1":
     objetos_seleccionados = objetos_disponibles
 elif opcion == "2":
-    objeto = input("Ingrese objeto (car, bicycle, traffic light, perso): ")
+    objeto = input("Ingrese objeto (car, bicycle, traffic light, person): ")
     objetos_seleccionados = [objeto]
 elif opcion == "3":
     objetos = input("Ingrese objetos separados por coma (car,bicycle,person): ")
@@ -35,7 +42,7 @@ for video in videos:
         if not ret:
             break
 
-        resultados = model(frame)
+        resultados = model(frame, verbose=False)
 
         for r in resultados:
             for box in r.boxes:
@@ -44,9 +51,10 @@ for video in videos:
 
                 if nombre in objetos_seleccionados:
                     x1, y1, x2, y2 = map(int, box.xyxy[0])
-                    cv2.rectangle(frame, (x1, y1), (x2, y2), (0,255,0), 2)
+                    color = colores.get(nombre, (255, 255, 255))
+                    cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
                     cv2.putText(frame, nombre, (x1, y1 - 10),
-                                cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0,255,0), 2)
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.9, color, 2)
 
         cv2.imshow("Reconocimiento", frame)
 
